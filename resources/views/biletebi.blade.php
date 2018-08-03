568317c285533650ae9c4ca1d7517395da924b31
Sandro <s.jijavadze@hotmail.com>
2018-08-03 21:29:42 +0400

    cleaned Libraries folder

--- resources/views/biletebi.blade.php
+++ resources/views/biletebi.blade.php
@@ -1,6 +1,24 @@
 @extends('layouts.app')
 @section('title', 'Train Tickets')
 @section('content')
-	{!! $ret !!}
+@if(is_array($ret))
+		<table class="table table-hover">		
+		<tr>
+			<thead class="thead-light">			
+				<th scope="col">Departure Date</th>
+				<th scope="col">Departure Time</th>
+				<th scope="col">Arrival Time</th>
+			</thead>
+		</tr>
+		@foreach($ret as $train)
+		<tr>         
+			<td>{{$train[0]}}</td>
+			<td>{{$train[1]}}</td>
+			<td>{{$train[2]}}</td>
+		</tr>
+			@endforeach
+@else
+		{!! $ret !!}
+@endif
 @endsection
 
